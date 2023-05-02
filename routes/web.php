155b5059a0037607c3a3b1b43cf302c7334937a7
Hakan Alif Pramudya <hakanalifpramudya13@gmail.com>
2023-05-02 20:42:18 +0700

    Praktikum 9: ORM dengan relasi

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routess
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
